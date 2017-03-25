c9662737ad796280a834b7fa5c9e77854077a988
Jack Linton <jacklinton@mac.com>
2017-03-25 13:40:12 -0400

    Locally installed bcrypt package, removed autopublish, moved control of collecting the tasks to the server, established an add-privacy function to posts with a button which allows users to hide certain posts from otehr users. Also installed the package classnames. Also added security measures to make sure that only the owner of posts can delete tasks or check them off.

--- imports/api/tasks.js
+++ imports/api/tasks.js
@@ -4,12 +4,25 @@ import { check } from 'meteor/check';
 
 export const Tasks = new Mongo.Collection('tasks');
 
+if (Meteor.isServer) {
+	// This code only runs on the server.
+	// Only publish tasks that are public or belong to the current user
+	Meteor.publish('tasks', function tasksPublication() {
+		return Tasks.find({
+			$or: [
+			{ private: { $ne: true } },
+			{ owner: this.userId },
+			],
+		});
+	});
+}
+
 Meteor.methods({
 	'tasks.insert'(text) {
 		check(text, String);
 
 		// Make sure the user is logged in before inserting a task
-		if  (! Metero.userId()) {
+		if  (! Meteor.userId()) {
 			throw new Metero.Error('not-authorized');
 		}
 
@@ -30,5 +43,18 @@ Meteor.methods({
 		check(setChecked, Boolean);
 
 		Tasks.update(taskId, { $set: { checked: setChecked } });
+	},
+	'tasks.setPrivate'(taskId, setToPrivate) {
+		check(taskId, String);
+		check(setToPrivate, Boolean);
+
+		const task = Tasks.findOne(taskId);
+
+		// Make sure only the task owner can make a task private
+		if  (task.owner !== Meteor.userId()) {
+			throw new Meteor.Error('not-authorized');
+		}
+
+		Tasks.update(taskId, { $set: { private: setToPrivate } });
 	},
 });
